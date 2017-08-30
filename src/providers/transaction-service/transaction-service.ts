5d7628e5432fa91de4ed81c0fd3eee7ee02f6723
Ed Zillion <edzillion@gmail.com>
2017-08-31 00:48:36 +0200

    moved all transact logs to backend

--- src/providers/transaction-service/transaction-service.ts
+++ src/providers/transaction-service/transaction-service.ts
@@ -2,12 +2,10 @@ import { Injectable, OnDestroy } from '@angular/core';
 import { Headers, Http } from '@angular/http';
 
 import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
-import * as firebase from 'firebase/app';
 import { Subscription } from 'rxjs/Subscription';
 
 import 'rxjs/add/operator/map';
 
-import { NewsService } from '../../providers/news-service/news-service';
 import { UserService } from '../../providers/user-service/user-service';
 import { User } from '../../interfaces/user-interface';
 
@@ -23,7 +21,6 @@ export class TransactionService implements OnDestroy {
   constructor(
     private db: AngularFireDatabase,
     private http: Http,
-    private newsService: NewsService,
     private userService: UserService
   ) {
 
@@ -35,24 +32,8 @@ export class TransactionService implements OnDestroy {
     this.transactionLog$ = this.db.list('/transactions/');
   }
 
-  private logTransfer(toUserKey:string, amount:number):void {
-
-    let logItem = {
-      "from" : this.user.uid,
-      "to" : toUserKey,
-      "timestamp" : firebase.database['ServerValue']['TIMESTAMP'],
-      "amount" : amount
-    } as LogItem;
-    //add to the main transaction log
-    this.transactionLog$.push(logItem);
-  }
-
   public transfer(fromUserKey,toUserKey,amount,message) {
-    return this.postTransaction(fromUserKey,toUserKey,amount).map(
-      (result) => {
-        this.newsService.addTransaction(toUserKey, amount, message);
-        return result;
-      }).first();
+    return this.postTransaction(fromUserKey,toUserKey,amount);
   }
 
   private postTransaction(fromUserKey,toUserKey,amount) {
